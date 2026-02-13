21f65e64c970918b89c96638cc855fae87470f8e
jehovany04 <jehovanykates04@gmail.com>
2026-02-13 13:47:21 +0100

    commit des fichiers 1 à 3

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,11 +1,24 @@
 #!/usr/bin/node
-// prints a square
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
 
-const size = parseInt(process.argv[2]);
-if (isNaN(size) || size <= 0) {
-  console.log('');
-} else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
-  }
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
